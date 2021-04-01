9beb484997a2cd743e1f8c68bd1c0b21f108b012
Michał Zając <diuf95o@gmail.com>
2021-04-01 13:04:20 +0200

    add api

--- movies_catalogue/main.py
+++ movies_catalogue/main.py
@@ -1,10 +1,74 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+
+import tmdb_client
+import requests
+
+def default_list(list):
+    try:
+        if list == 'popular' or  list == 'upcoming' or  list == 'now_playing' or  list == 'top_rated':
+            None
+        else:
+            list = 'popular'
+    except:
+        None
+    return list
+
+def select_list(selected_list):
+    default_class = "btn btn-outline-info"
+    buttons = [default_class,default_class,default_class,default_class]
+    if selected_list == 'popular':
+        buttons[0] = "btn btn-primary"
+    elif selected_list == 'upcoming':
+        buttons[1] = "btn btn-primary"
+    elif selected_list == 'now_playing':
+        buttons[2] = "btn btn-primary"
+    elif selected_list == 'top_rated':
+        buttons[3] = "btn btn-primary"
+    return buttons
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    selected_list = default_list(request.args.get('list_movies',"popular"))
+    movies = tmdb_client.get_movies(how_many=5,list_movies=selected_list)
+    buttons = select_list(selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list,buttons=buttons )
+
+
+@app.route('/movie/<int:movie_id>')
+def movie_details(movie_id):
+    movie = tmdb_client.get_details(movie_id)
+    actors = tmdb_client.get_credits(movie_id)["cast"][:4]
+    return render_template("details.html", movie=movie, actors=actors)
+
+
+@app.route('/search')
+def search():
+    search_query = request.args.get("query", "")
+    if search_query:
+        movies = tmdb_client.search_movies(search_query=search_query)["results"][:4]
+    else:
+        movies = []
+    return render_template("search.html", movies=movies, search_query=search_query)
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.context_processor
+def genre():
+    def genre_list(genres):
+        types = ""
+        for i in genres:
+            types +=f"{i['name']}, "
+        return types
+    return {"genre_list": genre_list}
 
 if __name__ == "__main__":
     app.run(debug=True)
